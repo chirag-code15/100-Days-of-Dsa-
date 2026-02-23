39dff0bdb38c9f8d8544d2d5208c1b8aea935b9d
chirag mahna <chiragmahna500@gmail.com>
2026-02-23 09:13:10 +0530

    Add files via upload

---
+++ 7.c
@@ -0,0 +1,14 @@
+// recursive aproach to  compute fibonacci nth term 
+#include<stdio.h>
+int fibonacci(int n){
+    if(n==0||n==1){
+        return n ; 
+    }
+    return fibonacci(n-1)+fibonacci(n-2) ; 
+}
+int main(){
+    int n ; 
+    scanf("%d",&n) ; 
+    printf("%d",fibonacci(n)) ; 
+    return 0 ; 
+}
